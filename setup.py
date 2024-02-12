1e94c04c67315a38c686384ff0f3d4cacc5d3f68
Philipp Rohde <Philipp.Rohde@tib.eu>
2024-02-12 13:20:58 +0100

    fix URL of project homepage

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/SDM-TIB/ckanext-dataexplorer-TIB',
+    url='https://github.com/SDM-TIB/ckanext-datacomparison',
 
     # Author details
     author='''Philipp D. Rohde''',
