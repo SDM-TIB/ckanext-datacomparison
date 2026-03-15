49446e2021bbca4979a2b508f5d725a4b2157546
Philipp Rohde <Philipp.Rohde@tib.eu>
2026-03-15 13:58:38 +0100

    add join column selection

--- ckanext/datacomparison/static/js/datacomparison.js
+++ ckanext/datacomparison/static/js/datacomparison.js
@@ -72,19 +72,125 @@ function addNewResourceForm() {
     details_height_update();
 }
 
+function applyJoin(data_new, columns_new_raw, join_col_old, join_col_new, res_new_label) {
+    if (num_resources === 2) {
+        join_column_name = join_col_old;
+        const elem_res1_label = document.getElementById('res1label'),
+              res1_label = ' (' + elem_res1_label.value + ')';
+        for (let i = 0; i < columns.length; i++) {
+            if (columns[i] !== join_col_old) { columns[i] = columns[i] + res1_label; }
+        }
+        elem_res1_label.disabled = true;
+    }
+
+    const idx_old = columns.indexOf(join_column_name),
+          idx_new = columns_new_raw.indexOf(join_col_new),
+          num_cols_old = columns.length - 1,
+          num_cols_new = columns_new_raw.length - 1;
+
+    for (let i = 0; i < columns_new_raw.length; i++) {
+        if (i !== idx_new) { columns_new_raw[i] = columns_new_raw[i] + res_new_label; }
+    }
+
+    let columns_copy = [...columns_new_raw];
+    columns_copy.splice(idx_new, 1);
+    columns = columns.concat(columns_copy);
+
+    while (data_new.length > 0) {
+        let found = false,
+            record = data_new.shift();
+        for (let i = 0; i < data_.length; i++) {
+            if (record[idx_new] == data_[i][idx_old]) {
+                found = true;
+                record.splice(idx_new, 1);
+
+                let length = record.length;
+                while (length < num_cols_new) {
+                    record.push(null);
+                    length++;
+                }
+
+                data_[i] = data_[i].concat(record);
+                break;
+            }
+        }
+        if (!found) {
+            let record_old = Array(num_cols_old + 1).fill(null);
+            record_old[idx_old] = record[idx_new];
+            record.splice(idx_new, 1);
+            record = record_old.concat(record);
+            data_.push(record);
+        }
+    }
+
+    const num_cols = num_cols_old + num_cols_new + 1;
+    for (let i = 0; i < data_.length; i++) {  // Take care of the old records which have not been updated yet...
+        if (data_[i].length < num_cols) { data_[i] = data_[i].concat(Array(num_cols - data_[i].length).fill(null)) }
+    }
+
+    addNewResourceForm();
+    updateUI();
+}
+
+function showJoinPicker(data_new, columns_new_raw, res_new_label) {
+    const backdrop = document.getElementById('join-modal-backdrop'),
+          select_old = document.getElementById('join_col_old_select'),
+          select_new = document.getElementById('join_col_new_select'),
+          old_select_wrapper = document.getElementById('join-modal-old-select-wrapper'),
+          old_info = document.getElementById('join-modal-old-info');
+
+    // Populate the selects with current column options
+    select_old.replaceChildren();
+    columns.forEach(col => {
+        let opt = document.createElement('option');
+        opt.value = col;
+        opt.text = col;
+        select_old.add(opt);
+    });
+
+    select_new.replaceChildren();
+    columns_new_raw.forEach(col => {
+        let opt = document.createElement('option');
+        opt.value = col;
+        opt.text = col;
+        select_new.add(opt);
+    });
+
+    // Toggle left-side state
+    if (num_resources === 2) {
+        old_select_wrapper.style.display = '';
+        old_info.style.display = 'none';
+    } else {
+        old_select_wrapper.style.display = 'none';
+        old_info.style.display = '';
+        document.getElementById('join-modal-old-info-text').innerText = join_column_name;
+    }
+
+    // Update the new-resource label to reflect the current resource number
+    document.getElementById('join-modal-new-label').innerText = 'Column from Resource ' + num_resources + ':';
+
+    // Wire up buttons (onclick assignment replaces any previous handler)
+    document.getElementById('join-modal-cancel').onclick = function() {
+        backdrop.style.display = 'none';
+    };
+    document.getElementById('join-modal-confirm').onclick = function() {
+        const col_old = num_resources === 2 ? select_old.value : join_column_name;
+        const col_new = select_new.value;
+        backdrop.style.display = 'none';
+        applyJoin(data_new, columns_new_raw, col_old, col_new, res_new_label);
+    };
+
+    backdrop.style.display = 'flex';
+}
+
 let num_resources = 2,
-    resource_list = document.getElementById('resource_list');
-const new_resource = document.getElementById('datasets');
+    resource_list = document.getElementById('resource_list'),
+    join_column_name = null,  // tracks the established join key across all resources
+    new_resource = document.getElementById('datasets');
 document.getElementById('res2').oninput = function() { return populateSearchBar(this.value) };
 new_resource.onsubmit = function(event) {
     event.preventDefault();
 
-    if (num_resources === 2) {
-        const elem_res1_label = document.getElementById('res1label'),
-              res1_label = ' (' + elem_res1_label.value + ')';
-        for (let i = 1; i < columns.length; i++) { columns[i] = columns[i] + res1_label }
-        elem_res1_label.disabled = true;
-    }
     const res_new = document.getElementById('res' + num_resources).value,
           res_new_label = ' (' + document.getElementById('res' + num_resources + 'label').value + ')';
 
@@ -92,60 +198,10 @@ new_resource.onsubmit = function(event) {
         .then(res => res.text())
         .then(data => {
             let data_new = Papa.parse(data, { skipEmptyLines: 'greedy' }).data,
-                columns_new = data_new.shift();
-            for (let i = 1; i < columns_new.length; i++) { columns_new[i] = columns_new[i] + res_new_label }
-
-            const intersection = columns.filter(value => columns_new.includes(value));
-            if (1 === 1) {
-                const join_column = intersection[0],
-                    idx_old = 0, //columns.indexOf(join_column),
-                    idx_new = 0, //columns_new.indexOf(join_column),
-                    num_cols_old = columns.length - 1,
-                    num_cols_new = columns_new.length - 1;
-
-                let columns_copy = [...columns_new];
-                columns_copy.splice(idx_new, 1);
-                columns = columns.concat(columns_copy);
-
-                while (data_new.length > 0) {
-                    let found = false,
-                        record = data_new.shift();
-                    for (let i = 0; i < data_.length; i++) {
-                        if (record[idx_new] == data_[i][idx_old]) {
-                            found = true;
-                            record.splice(idx_new, 1);
-
-                            let length = record.length;
-                            while (length < num_cols_new) {
-                                record.push(null);
-                                length++;
-                            }
-
-                            data_[i] = data_[i].concat(record);
-                            break;
-                        }
-                    }
-                    if (!found) {
-                        let record_old = Array(num_cols_old + 1).fill(null);
-                        record_old[idx_old] = record[idx_new];
-                        record.splice(idx_new, 1);
-                        record = record_old.concat(record);
-                        data_.push(record);
-                    }
-                }
-
-                const num_cols = num_cols_old + num_cols_new + 1;
-                for (let i = 0; i < data_.length; i++) {  // Take care of the old records which have not been updated yet...
-                    if (data_[i].length < num_cols) { data_[i] = data_[i].concat(Array(num_cols - data_[i].length).fill(null)) }
-                }
-
-                addNewResourceForm();
-            } else {
-                console.log('There is either no or several matching column(s).')
-            }
+                columns_new_raw = data_new.shift();
+            showJoinPicker(data_new, columns_new_raw, res_new_label);
         })
-        .catch(err => console.error(err))
-        .then(_ => updateUI());
+        .catch(err => console.error(err));
 };
 
 function populateSearchBar(name) {
